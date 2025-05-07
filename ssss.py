4654c6758daddce9be852db25212031f89fcd9ae
Kseniia Barashok <kseniiamaus@gmail.com>
2025-05-07 22:59:00 +0900

    ssqwt and signal restore

--- ssss.py
+++ ssss.py
@@ -3,6 +3,7 @@ import numpy as np
 from ssqueezepy import ssq_cwt, issq_cwt, extract_ridges
 from ssqueezepy.visuals import imshow, plot, scat
 from ssqueezepy.toolkit import lin_band
+from ssqueezepy.visuals import imshow, plot
 import matplotlib.pyplot as plt
 import cv2
 
@@ -28,7 +29,8 @@ print(freqband)
 # TODO: the task is to define Cs and freqband automatically (not using lin_band)
 
 #%%###########################################################################
-kw = dict(wavelet=('morlet', {'mu': 4.5}), nv=32, scales='log')
+morlet_mu = 4.5
+kw = dict(wavelet=('morlet', {'mu': morlet_mu}), nv=32, scales='log')
 xrec = issq_cwt(Tx, kw['wavelet'], Cs, freqband)[0]
 
 dt = time[1]
@@ -61,22 +63,30 @@ areas = stats[1:, cv2.CC_STAT_AREA]
 sorted_indices = np.argsort(-areas)[:2]
 
 plt.figure(figsize=(6, 6))
-plt.imshow(mask, aspect='auto', origin='lower', cmap='gray')
+# plt.imshow(mask, aspect='auto', origin='lower', cmap='gray')
 for i, idx in enumerate(sorted_indices):
     x, y, w, h, area = stats[idx + 1]
     max_x, min_x = x + w, x
     max_y, min_y = y + h, y
     print(f'Object {i + 1}: minX={min_x}, maxX={max_x}, minY={min_y}, maxY={max_y}')
-    plt.gca().add_patch(plt.Rectangle((x, y), w, h, edgecolor='red', facecolor='none', linewidth=2))
-    plt.text(x, y - 5, str(i + 1), color='yellow', fontsize=12, weight='bold')
+    # plt.gca().add_patch(plt.Rectangle((x, y), w, h, edgecolor='red', facecolor='none', linewidth=2))
+    # plt.text(x, y - 5, str(i + 1), color='yellow', fontsize=12, weight='bold')
 
+i = 0
+idx = sorted_indices[i]
+x, y, w, h, area = stats[idx + 1]
+bbox_mask = np.zeros_like(Tx, dtype=bool)
+bbox_mask[y:y+h, x:x+w] = True
 
-plt.title(f'Mask > {threshold_percent*100:.0f}%')
-plt.tight_layout()
-plt.gca().invert_yaxis()
+Tx_i = np.where(bbox_mask, Tx, 0+0j)
+imshow(Tx_i)
+# plt.title(f'Mask > {threshold_percent*100:.0f}%')
+# plt.title(f'Tx_{i}')
+# plt.tight_layout()
+# plt.gca().invert_yaxis()
+# plt.show()
+
+xrec = issq_cwt(Tx_i, wavelet=('morlet', {'mu': 4.5}))
+plt.plot(xrec)
 plt.show()
-
-
-
-
 
